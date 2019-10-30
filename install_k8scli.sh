01efbecb2ab48051c3d19aab5dc54854d2ec7abf
yuhkih <yuhkih@hotmail.com>
2019-10-30 15:48:05 +0900

    workable files

---
+++ install_k8scli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# K8S CLI install for Linux
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# check if kubectl is installed successfully
+kubectl version
