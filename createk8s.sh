fc61e181dad467ba2c140bece8a48bb28d01da0e
yuhkih <yuhkih@hotmail.com>
2019-11-08 20:10:48 +0900

    small changes

--- createk8s.sh
+++ createk8s.sh
@@ -37,31 +37,31 @@ echo  "[" $shellname "] create resource group for registry"
 result=`az group create --resource-group $ACR_RES_GROUP --location japaneast`
 ret=$?
 echo  "[" $shellname "] create resource group for registry result =" $result "," "return code=" $ret
-echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
+echo  "[" $shellname "] Sleep 10 sec to make sure az command completion"; sleep 10
 
 # -- レジストリの作成
 echo "[" $shellname "] create registry"
 result=`az acr create --resource-group $ACR_RES_GROUP --name $ACR_NAME --sku Standard --location japaneast`
 ret=$?
 echo  "[" $shellname "] create registry result =" $result "," "return code=" $ret
-echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
+echo  "[" $shellname "] Sleep 10 sec to make sure az command completion"; sleep 10
 
 # -- レジストリへのイメージの登録 1
 echo "[" $shellname "] build registry for v1.0"
 result=`az acr build --registry $ACR_NAME --image photo-view:v1.0 Understanding-K8s/chap02/v1.0/`
 ret=$?
-echo  "[" $shellname "] build registry result =" $result "," "return code=" $ret
-echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
+echo "[" $shellname "] build registry result =" $result "," "return code=" $ret
+echo "[" $shellname "] Sleep 10 sec to make sure az command completion"; sleep 10
 
 # -- レジストリへのイメージの登録 2
 echo "[" $shellname "] build registry for v2.0"
 result=`az acr build --registry $ACR_NAME --image photo-view:v2.0 Understanding-K8s/chap02/v2.0/`
 ret=$?
-echo  "[" $shellname "] build registry result =" $result "," "return code=" $ret
-echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
+echo "[" $shellname "] build registry result =" $result "," "return code=" $ret
+echo "[" $shellname "] Sleep 10 sec to make sure az command completion"; sleep 10
 
 # -- リポジトリの確認
-echo  "[" $shellname "] check if repository are created "
+echo "[" $shellname "] check if repository are created "
 az acr repository show-tags -n $ACR_NAME --repository photo-view
 
 # -- ACR と AKS の連携のためのID / Password の作成
@@ -71,35 +71,36 @@ SP_PASSWD=$(az ad sp create-for-rbac --name $SP_NAME --role Reader --scopes $ACR
 APP_ID=$(az ad sp show --id http://$SP_NAME --query appId --output tsv)
 
 # -- 確認
-echo  "[" $shellname "] APP_ID = " $APP_ID
-echo  "[" $shellname "] SP_PASSWD = " $SP_PASSWD
+echo "[" $shellname "] APP_ID = " $APP_ID
+echo "[" $shellname "] SP_PASSWD = " $SP_PASSWD
 
 #  - リソースグループの作成
-echo  "[" $shellname "] Creating K8S cluster resource group"
+echo "[" $shellname "] Creating K8S cluster resource group"
 result=`az group create --resource-group $AKS_RES_GROUP --location japaneast`
 ret=$?
-echo  "[" $shellname "] Creating K8S Cluster resource group =" $result "," "return code=" $ret
-echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
-
+echo "[" $shellname "] Creating K8S Cluster resource group =" $result "," "return code=" $ret
+echo "[" $shellname "] Sleep 10 sec to make sure az command completion"; sleep 10
 
 # -- K8S クラスタの作成
-echo  "[" $shellname "] Creating K8S cluster"
+echo "[" $shellname "] Creating K8S cluster. This takes time."
+echo "[" $shellname "] Start creating K8S cluster: " `date '+%y/%m/%d %H:%M:%S'`
 result=`az aks create --name $AKS_CLUSTER_NAME --resource-group $AKS_RES_GROUP --node-count 3 --kubernetes-version 1.12.8 --node-vm-size Standard_DS1_v2 --generate-ssh-keys --service-principal $APP_ID --client-secret $SP_PASSWD`
-ret=$?
-echo  "[" $shellname "] Creating K8S Cluster result =" $result "," "return code=" $ret
-echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
+ret=$? 
+echo "[" $shellname "] Creating K8S Cluster result =" $result "," "return code=" $ret
+echo "[" $shellname "] Sleep 10 sec to make sure az command completion"; sleep 10
+echo "[" $shellname "] End creaating K8S Cluster: " `date '+%y/%m/%d %H:%M:%S'`
   
-# -クレデンシャルの取得
-echo  "[" $shellname "] Getting credentials" 
+# -- クレデンシャルの取得
+echo "[" $shellname "] Getting credentials" 
 result=`az aks get-credentials --admin --resource-group $AKS_RES_GROUP --name $AKS_CLUSTER_NAME`
 ret=$?
-echo  "[" $shellname "] Getting credentials result =" $result "," "return code=" $ret
-echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
+echo "[" $shellname "] Getting credentials result =" $result "," "return code=" $ret
+echo "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
 
 # --確認コマンド
 echo "[" $shellname "] check if cluster is created successfully"
 kubectl cluster-info
 # kubectl get node
-echo  "[" $shellname "] cluster node check"
+echo "[" $shellname "] cluster node check"
 kubectl get node -o=wide
 # kubectl describe node aks-nodepool1-36860460-0
--- deletek8s.sh
+++ deletek8s.sh
@@ -3,9 +3,20 @@ export SP_NAME=sample-acr-service-principal
 export ACR_RES_GROUP=yuhkiACRRegistry
 export AKS_RESOURCE_GROUP=AKSCluster
 
-echo "Deleting Azure Container Resource Group"
-az group delete --name $ACR_RES_GROUP --yes
-echo "Deleting Azure K8S Resource Group"
-az group delete --name $AKS_RESOURCE_GROUP --yes
-echo "Deleting Azure Service Principal"
-az ad sp delete --id=$(az ad sp show --id http://$SP_NAME --query appId --output tsv)
+# -- シェル名の取得
+shellname=${0##*/}
+
+echo "[" $shellname "] Deleting Azure Container Resource Group"
+result=`az group delete --name $ACR_RES_GROUP --yes`
+ret=$?
+echo  "[" $shellname "] Deleting Azure Container Resource Group result =" $result "," "return code=" $ret
+
+echo "[" $shellname "] Deleting Azure K8S Resource Group"
+result=`az group delete --name $AKS_RESOURCE_GROUP --yes`
+ret=$?
+echo  "[" $shellname "] Deleting Azure K8S Resource Group result =" $result "," "return code=" $ret
+
+echo "[" $shellname "] Deleting Azure Service Principal"
+result=`az ad sp delete --id=$(az ad sp show --id http://$SP_NAME --query appId --output tsv)`
+ret=$?
+echo  "[" $shellname "] Deleting Azure Service Principal result =" $result "," "return code=" $ret
