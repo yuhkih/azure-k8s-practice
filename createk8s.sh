41c88655e571fa7e11c2f32c2e5d9fa0d08ac05b
yuhkih <yuhkih@hotmail.com>
2019-11-08 19:39:08 +0900

    small changes

--- createk8s.sh
+++ createk8s.sh
@@ -16,11 +16,11 @@ export AKS_RES_GROUP=$AKS_CLUSTER_NAME
 export SP_NAME=sample-acr-service-principal
 
 
-echo "The folloing values will be used for k8s creation"
-echo  "ACR_RES_GROUP = " $ACR_RES_GROUP
-echo "AKS_CLUSTER_NAME = " $AKS_CLUSTER_NAME
-echo  "ACR_NAME =" $ACR_NAME
-echo  "AKS_RES_GROUP = " $AKS_RES_GROUP
+echo  "[" $shellname "] The folloing values will be used for k8s creation"
+echo  "[" $shellname "] ACR_RES_GROUP = " $ACR_RES_GROUP
+echo  "[" $shellname "] AKS_CLUSTER_NAME = " $AKS_CLUSTER_NAME
+echo  "[" $shellname "] ACR_NAME =" $ACR_NAME
+echo  "[" $shellname "] AKS_RES_GROUP = " $AKS_RES_GROUP
 
 # -- サンプルのダウンロード
 if [ ! -d Understanding-K8s ]
@@ -28,14 +28,40 @@ then
     git clone https://github.com/ToruMakabe/Understanding-K8s
 fi
 
+# -- シェル名の取得
+shellname=${0##*/}
+
 # -- レジストリにコンテナを登録
-az group create --resource-group $ACR_RES_GROUP --location japaneast
-az acr create --resource-group $ACR_RES_GROUP --name $ACR_NAME --sku Standard --location japaneast
-az acr build --registry $ACR_NAME --image photo-view:v1.0 Understanding-K8s/chap02/v1.0/
-az acr build --registry $ACR_NAME --image photo-view:v2.0 Understanding-K8s/chap02/v2.0/
+# -- リソース・グループの作成
+echo  "[" $shellname "] create resource group for registry"
+result=`az group create --resource-group $ACR_RES_GROUP --location japaneast`
+ret=$?
+echo  "[" $shellname "] create resource group for registry result =" $result "," "return code=" $ret
+echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
+
+# -- レジストリの作成
+echo "[" $shellname "] create registry"
+result=`az acr create --resource-group $ACR_RES_GROUP --name $ACR_NAME --sku Standard --location japaneast`
+ret=$?
+echo  "[" $shellname "] create registry result =" $result "," "return code=" $ret
+echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
+
+# -- レジストリへのイメージの登録 1
+echo "[" $shellname "] build registry for v1.0"
+result=`az acr build --registry $ACR_NAME --image photo-view:v1.0 Understanding-K8s/chap02/v1.0/`
+ret=$?
+echo  "[" $shellname "] build registry result =" $result "," "return code=" $ret
+echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
+
+# -- レジストリへのイメージの登録 2
+echo "[" $shellname "] build registry for v2.0"
+result=`az acr build --registry $ACR_NAME --image photo-view:v2.0 Understanding-K8s/chap02/v2.0/`
+ret=$?
+echo  "[" $shellname "] build registry result =" $result "," "return code=" $ret
+echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
 
 # -- リポジトリの確認
-echo "check if repository are created "
+echo  "[" $shellname "] check if repository are created "
 az acr repository show-tags -n $ACR_NAME --repository photo-view
 
 # -- ACR と AKS の連携のためのID / Password の作成
@@ -45,25 +71,35 @@ SP_PASSWD=$(az ad sp create-for-rbac --name $SP_NAME --role Reader --scopes $ACR
 APP_ID=$(az ad sp show --id http://$SP_NAME --query appId --output tsv)
 
 # -- 確認
-echo "APP_ID = " $APP_ID
-echo "SP_PASSWD = " $SP_PASSWD
+echo  "[" $shellname "] APP_ID = " $APP_ID
+echo  "[" $shellname "] SP_PASSWD = " $SP_PASSWD
 
 #  - リソースグループの作成
-echo "Creating K8S cluster resource group"
-az group create --resource-group $AKS_RES_GROUP --location japaneast
+echo  "[" $shellname "] Creating K8S cluster resource group"
+result=`az group create --resource-group $AKS_RES_GROUP --location japaneast`
+ret=$?
+echo  "[" $shellname "] Creating K8S Cluster resource group =" $result "," "return code=" $ret
+echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
 
-# -- クラスタの作成
-echo "Creating K8S cluster"
-az aks create --name $AKS_CLUSTER_NAME --resource-group $AKS_RES_GROUP --node-count 3 --kubernetes-version 1.12.8 --node-vm-size Standard_DS1_v2 --generate-ssh-keys --service-principal $APP_ID --client-secret $SP_PASSWD
+
+# -- K8S クラスタの作成
+echo  "[" $shellname "] Creating K8S cluster"
+result=`az aks create --name $AKS_CLUSTER_NAME --resource-group $AKS_RES_GROUP --node-count 3 --kubernetes-version 1.12.8 --node-vm-size Standard_DS1_v2 --generate-ssh-keys --service-principal $APP_ID --client-secret $SP_PASSWD`
+ret=$?
+echo  "[" $shellname "] Creating K8S Cluster result =" $result "," "return code=" $ret
+echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
   
 # -クレデンシャルの取得
-echo "Getting credentials" 
-az aks get-credentials --admin --resource-group $AKS_RES_GROUP --name $AKS_CLUSTER_NAME
+echo  "[" $shellname "] Getting credentials" 
+result=`az aks get-credentials --admin --resource-group $AKS_RES_GROUP --name $AKS_CLUSTER_NAME`
+ret=$?
+echo  "[" $shellname "] Getting credentials result =" $result "," "return code=" $ret
+echo  "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
 
 # --確認コマンド
-echo "check if cluster is created successfully"
+echo "[" $shellname "] check if cluster is created successfully"
 kubectl cluster-info
 # kubectl get node
-echo "cluster node check"
+echo  "[" $shellname "] cluster node check"
 kubectl get node -o=wide
 # kubectl describe node aks-nodepool1-36860460-0
--- deletek8s.sh
+++ deletek8s.sh
@@ -4,8 +4,8 @@ export ACR_RES_GROUP=yuhkiACRRegistry
 export AKS_RESOURCE_GROUP=AKSCluster
 
 echo "Deleting Azure Container Resource Group"
-az group delete --name $ACR_RES_GROUP
+az group delete --name $ACR_RES_GROUP --yes
 echo "Deleting Azure K8S Resource Group"
-az group delete --name $AKS_RESOURCE_GROUP
+az group delete --name $AKS_RESOURCE_GROUP --yes
 echo "Deleting Azure Service Principal"
 az ad sp delete --id=$(az ad sp show --id http://$SP_NAME --query appId --output tsv)
