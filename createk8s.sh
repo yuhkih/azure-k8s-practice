946d5f85f452fe1d99245ab8b0fb3a3be4eb1ded
yuhkih <yuhki@yuhki-legion.localdomain>
2019-11-09 11:48:56 +0900

    small updates

--- createk8s.sh
+++ createk8s.sh
@@ -4,7 +4,7 @@
 # login to Azure
 az login
 
-# -- 環境変数
+# -- 環境変数 --
 # ACR = Azure Container Registry の略
 # ここの名前は自分用に変更して下さい (ACR_RES_GROUP / AKS_CLUSTER_NAME / SP_NAME)
 export ACR_RES_GROUP=yuhkiACRRegistry
@@ -15,12 +15,20 @@ export AKS_RES_GROUP=$AKS_CLUSTER_NAME
 
 export SP_NAME=sample-acr-service-principal
 
+# -- シェル名の取得 --
+shellname=${0##*/}
 
-echo  "[" $shellname "] The folloing values will be used for k8s creation"
-echo  "[" $shellname "] ACR_RES_GROUP = " $ACR_RES_GROUP
-echo  "[" $shellname "] AKS_CLUSTER_NAME = " $AKS_CLUSTER_NAME
-echo  "[" $shellname "] ACR_NAME =" $ACR_NAME
-echo  "[" $shellname "] AKS_RES_GROUP = " $AKS_RES_GROUP
+# -- ログ用のファンクション -- 
+function shell_log(){
+    shellname=${0##*/}
+    echo  "[" $shellname "][" `date '+%Y/%m/%d %H:%M:%S'`" ] " $1 $2 $3 $4
+}
+
+shell_log "The folloing values will be used for k8s creation"
+shell_log "ACR_RES_GROUP = " $ACR_RES_GROUP
+shell_log "AKS_CLUSTER_NAME = " $AKS_CLUSTER_NAME
+shell_log "ACR_NAME =" $ACR_NAME
+shell_log "AKS_RES_GROUP = " $AKS_RES_GROUP
 
 # -- サンプルのダウンロード
 if [ ! -d Understanding-K8s ]
@@ -28,41 +36,44 @@ then
     git clone https://github.com/ToruMakabe/Understanding-K8s
 fi
 
-# -- シェル名の取得
-shellname=${0##*/}
-
 # -- レジストリにコンテナを登録
 # -- リソース・グループの作成
-echo  "[" $shellname "] create resource group for registry"
-result=`az group create --resource-group $ACR_RES_GROUP --location japaneast`
+command="az group create --resource-group $ACR_RES_GROUP --location japaneast"
+shell_log "レジストリのリスースグループを作成します ["  "$command" "]"
+result=eval $command
 ret=$?
-echo  "[" $shellname "] create resource group for registry result =" $result "," "return code=" $ret
-echo  "[" $shellname "] Sleep 10 sec to make sure az command completion"; sleep 10
+shell_log "create resource group for registry result=[" $result "]," "return code=" $ret
+shell_log "Sleep 10 sec to make sure az command completion"; sleep 10
 
 # -- レジストリの作成
-echo "[" $shellname "] create registry"
-result=`az acr create --resource-group $ACR_RES_GROUP --name $ACR_NAME --sku Standard --location japaneast`
+command="az acr create --resource-group $ACR_RES_GROUP --name $ACR_NAME --sku Standard --location japaneast"
+shell_log "レジストリを作成します ["　"$command" "]"
+result=eval $command
 ret=$?
-echo  "[" $shellname "] create registry result =" $result "," "return code=" $ret
-echo  "[" $shellname "] Sleep 10 sec to make sure az command completion"; sleep 10
+shell_log "create registry result =" $result "," "return code=" $ret
+shell_log "Sleep 10 sec to make sure az command completion"; sleep 10
 
 # -- レジストリへのイメージの登録 1
-echo "[" $shellname "] build registry for v1.0"
-result=`az acr build --registry $ACR_NAME --image photo-view:v1.0 Understanding-K8s/chap02/v1.0/`
+command="az acr build --registry $ACR_NAME --image photo-view:v1.0 Understanding-K8s/chap02/v1.0/"
+shell_log "v1.0 用のコンテナイメージをビルドしてレジストリに登録します。 [" "$command" "]"
+result=eval $command
 ret=$?
-echo "[" $shellname "] build registry result =" $result "," "return code=" $ret
-echo "[" $shellname "] Sleep 10 sec to make sure az command completion"; sleep 10
+shell_log "build registry result=[" $result "]," "return code=" $ret
+shell_log "Sleep 10 sec to make sure az command completion"; sleep 10
 
 # -- レジストリへのイメージの登録 2
-echo "[" $shellname "] build registry for v2.0"
-result=`az acr build --registry $ACR_NAME --image photo-view:v2.0 Understanding-K8s/chap02/v2.0/`
+command="az acr build --registry $ACR_NAME --image photo-view:v2.0 Understanding-K8s/chap02/v2.0/"
+shell_log "v2.0用のコンテナイメージをビルドしてレジストリに登録します。[" "$command" "]"
+result=eval $command
 ret=$?
-echo "[" $shellname "] build registry result =" $result "," "return code=" $ret
-echo "[" $shellname "] Sleep 10 sec to make sure az command completion"; sleep 10
+shell_log "build registry result=[" $result "]," "return code=" $ret
+shell_log "Sleep 10 sec to make sure az command completion"; sleep 10
 
 # -- リポジトリの確認
-echo "[" $shellname "] check if repository are created "
-az acr repository show-tags -n $ACR_NAME --repository photo-view
+command="az acr repository show-tags -n $ACR_NAME --repository photo-view"
+shell_log "レポジトリが作成されたか確認します。 [" "$command" "]"
+result=eval $command
+ret=$?
 
 # -- ACR と AKS の連携のためのID / Password の作成
 # AKS は、ACR からコンテナを取得する
@@ -71,38 +82,38 @@ SP_PASSWD=$(az ad sp create-for-rbac --name $SP_NAME --role Reader --scopes $ACR
 APP_ID=$(az ad sp show --id http://$SP_NAME --query appId --output tsv)
 
 # -- 確認
-echo "[" $shellname "] APP_ID = " $APP_ID
-echo "[" $shellname "] SP_PASSWD = " $SP_PASSWD
+shell_log "APP_ID = " $APP_ID
+shell_log "SP_PASSWD = " $SP_PASSWD
 
 #  - リソースグループの作成
-echo "[" $shellname "] Creating K8S cluster resource group"
-result=`az group create --resource-group $AKS_RES_GROUP --location japaneast`
+command="az group create --resource-group $AKS_RES_GROUP --location japaneast"
+shell_log "K8S クラスタ用のリソースグループを作成します。 [" "$command" "]"
+result=eval $command
 ret=$?
-echo "[" $shellname "] Creating K8S Cluster resource group =" $result "," "return code=" $ret
-echo "[" $shellname "] Sleep 10 sec to make sure az command completion"; sleep 10
+shell_log "Creating K8S Cluster resource group=[" $result "]," "return code=" $ret
+shell_log "Sleep 10 sec to make sure az command completion"; sleep 10
 
 # -- K8S クラスタの作成
-echo "[" $shellname "] Creating K8S cluster. This takes time."
-echo "[" $shellname "] Start creating K8S cluster: " `date '+%y/%m/%d %H:%M:%S'`
-result=`az aks create --name $AKS_CLUSTER_NAME --resource-group $AKS_RES_GROUP --node-count 3 --kubernetes-version 1.12.8 --node-vm-size Standard_DS1_v2 --generate-ssh-keys --service-principal $APP_ID --client-secret $SP_PASSWD`
-ret=$? 
-echo "[" $shellname "] Creating K8S Cluster result =" $result "," "return code=" $ret
-echo "[" $shellname "] Sleep 10 sec to make sure az command completion"; sleep 10
-echo "[" $shellname "] End creaating K8S Cluster: " `date '+%y/%m/%d %H:%M:%S'`
-  
-# -- クレデンシャルの取得
-echo "[" $shellname "] Getting credentials. This takes time." 
-echo "[" $shellname "] Start getting credentials: " `date '+%y/%m/%d %H:%M:%S'`
-result=`az aks get-credentials --admin --resource-group $AKS_RES_GROUP --name $AKS_CLUSTER_NAME`
+command="az aks create --name $AKS_CLUSTER_NAME --resource-group $AKS_RES_GROUP --node-count 3 --kubernetes-version 1.12.8 --node-vm-size Standard_DS1_v2 --generate-ssh-keys --service-principal $APP_ID --client-secret $SP_PASSWD"
+shell_log "K8Sのクラスタを作成します。この処理は少し時間がかかります。 [" "$command" "]"
+result=eval $command
 ret=$?
-echo "[" $shellname "] Getting credentials result =" $result "," "return code=" $ret
-echo "[" $shellname "] End getting credentials: " `date '+%y/%m/%d %H:%M:%S'`
-echo "[" $shellname "] Sleep 10 sec for az command completion"; sleep 10
+shell_log "Creating K8S Cluster result=[" $result "]," "return code=" $ret
+shell_log "Sleep 30 sec to make sure az command completion"; sleep 30
+shell_log "End creaating K8S Cluster: " `date '+%y/%m/%d %H:%M:%S'`
+  
+# -- クレデンシャルの取得 ./kube にクレデンシャル情報が書き込まれて kubectl が使えるようになる。
+command="az aks get-credentials --admin --resource-group $AKS_RES_GROUP --name $AKS_CLUSTER_NAME --overwrite-existing"
+shell_log "kubectl 用のクレデンシャルを取得します。["  "$command" "]"
+result=eval $command
+ret=$?
+shell_log "Getting credentials result =" $result "," "return code=" $ret
+shell_Log  "Sleep 10 sec for az command completion"; sleep 10
 
 # --確認コマンド
-echo "[" $shellname "] check if cluster is created successfully"
+shell_log "check if cluster is created successfully"
 kubectl cluster-info
 # kubectl get node
-echo "[" $shellname "] cluster node check"
+shell_log "cluster node check"
 kubectl get node -o=wide
 # kubectl describe node aks-nodepool1-36860460-0
--- deletek8s.sh
+++ deletek8s.sh
@@ -3,20 +3,33 @@ export SP_NAME=sample-acr-service-principal
 export ACR_RES_GROUP=yuhkiACRRegistry
 export AKS_RESOURCE_GROUP=AKSCluster
 
-# -- シェル名の取得
+# -- シェル名の取得 --
 shellname=${0##*/}
 
-echo "[" $shellname "] Deleting Azure Container Resource Group"
-result=`az group delete --name $ACR_RES_GROUP --yes`
-ret=$?
-echo  "[" $shellname "] Deleting Azure Container Resource Group result =" $result "," "return code=" $ret
+# -- ログ用のファンクション -- 
+function shell_log(){
+    shellname=${0##*/}
+    echo  "[" $shellname "][" `date '+%Y/%m/%d %H:%M:%S'`" ] " $1 $2 $3 $4 $5
+}
 
-echo "[" $shellname "] Deleting Azure K8S Resource Group"
-result=`az group delete --name $AKS_RESOURCE_GROUP --yes`
+command="az group delete --name $ACR_RES_GROUP --yes"
+shell_log "Deleting Azure Container Resource Group:" "$command"
+result=eval $command
 ret=$?
-echo  "[" $shellname "] Deleting Azure K8S Resource Group result =" $result "," "return code=" $ret
+shell_log "Deleting Azure Container Resource Group result =[" $result "]," "return code=" $ret
 
-echo "[" $shellname "] Deleting Azure Service Principal"
-result=`az ad sp delete --id=$(az ad sp show --id http://$SP_NAME --query appId --output tsv)`
+command="az group delete --name $AKS_RESOURCE_GROUP --yes"
+shell_log "Deleting Azure K8S Resource Group:" "$command"
+result=eval $command
 ret=$?
-echo  "[" $shellname "] Deleting Azure Service Principal result =" $result "," "return code=" $ret
+shell_log "Deleting Azure K8S Resource Group result =[" $result "]," "return code=" $ret
+
+# command="az ad sp delete --id=$(az ad sp show --id http://$SP_NAME --query appId --output tsv)" # これは one line では代入できない。
+spid=`az ad sp show --id http://$SP_NAME --query appId --output tsv`
+# echo $spid
+command="az ad sp delete --id=$spid"
+echo $command
+shell_log "Deleting Azure Service Principal:" "$command"
+result=eval $command
+ret=$?
+shell_log "Deleting Azure Service Principal result =[" $result "]," "return code=" $ret
