b7ad8949c16afabf717feaaa565b946157939ecb
yuhkih <yuhki@yuhki-legion.localdomain>
2019-12-13 16:00:59 +0900

    change vm for k8s to smaller ones which are covered by 750 horus free

--- createk8s.sh
+++ createk8s.sh
@@ -15,6 +15,8 @@ export AKS_RES_GROUP=$AKS_CLUSTER_NAME
 
 export SP_NAME=sample-acr-service-principal
 
+START_TIME=`date '+%Y/%m/%d %H:%M:%S'`
+
 # -- シェル名の取得 --
 shellname=${0##*/}
 
@@ -39,7 +41,7 @@ fi
 # -- レジストリにコンテナを登録
 # -- リソース・グループの作成
 command="az group create --resource-group $ACR_RES_GROUP --location japaneast"
-shell_log "レジストリのリスースグループを作成します ["  "$command" "]"
+shell_log "レジストリのリソース・グループを作成します ["  "$command" "]"
 result=eval $command
 ret=$?
 shell_log "create resource group for registry result=[" $result "]," "return code=" $ret
@@ -47,7 +49,7 @@ shell_log "Sleep 10 sec to make sure az command completion"; sleep 10
 
 # -- レジストリの作成
 command="az acr create --resource-group $ACR_RES_GROUP --name $ACR_NAME --sku Standard --location japaneast"
-shell_log "レジストリを作成します ["　"$command" "]"
+shell_log "コンテナを登録するためのレジストリを作成します ["　"$command" "]"
 result=eval $command
 ret=$?
 shell_log "create registry result =" $result "," "return code=" $ret
@@ -75,13 +77,14 @@ shell_log "レポジトリが作成されたか確認します。 [" "$command" 
 result=eval $command
 ret=$?
 
-# -- ACR と AKS の連携のためのID / Password の作成
+# --　AKSやACRを動的に操作するための Service Principalを作成。 Service Principal の Password の作成
 # AKS は、ACR からコンテナを取得する
 ACR_ID=$(az acr show --name $ACR_NAME --query id --output tsv)
 SP_PASSWD=$(az ad sp create-for-rbac --name $SP_NAME --role Reader --scopes $ACR_ID --query password --output tsv)
 APP_ID=$(az ad sp show --id http://$SP_NAME --query appId --output tsv)
 
 # -- 確認
+shell_log "以下の情報を使って、AKSは、ACRからコンテナを取得します。"
 shell_log "APP_ID = " $APP_ID
 shell_log "SP_PASSWD = " $SP_PASSWD
 
@@ -94,21 +97,25 @@ shell_log "Creating K8S Cluster resource group=[" $result "]," "return code=" $r
 shell_log "Sleep 10 sec to make sure az command completion"; sleep 10
 
 # -- K8S クラスタの作成
-command="az aks create --name $AKS_CLUSTER_NAME --resource-group $AKS_RES_GROUP --node-count 3 --kubernetes-version 1.12.8 --node-vm-size Standard_DS1_v2 --generate-ssh-keys --service-principal $APP_ID --client-secret $SP_PASSWD"
+# DS1 v2 (1vCPU/3.5GiB Mem/7GiB Disk) 6 yen /hour
+# command="az aks create --name $AKS_CLUSTER_NAME --resource-group $AKS_RES_GROUP --node-count 3 --kubernetes-version 1.12.8 --node-vm-size Standard_DS1_v2 --generate-ssh-keys --service-principal $APP_ID --client-secret $SP_PASSWD"
+# B1S (1vCPU/1GiB Mem/4GiB Disk) Linux is 750 hours free.  or 1.1 yen /hour
+command="az aks create --name $AKS_CLUSTER_NAME --resource-group $AKS_RES_GROUP --node-count 3 --kubernetes-version 1.12.8 --node-vm-size Standard_B1s --generate-ssh-keys --service-principal $APP_ID --client-secret $SP_PASSWD"
+
 shell_log "K8Sのクラスタを作成します。この処理は少し時間がかかります。 [" "$command" "]"
 result=eval $command
 ret=$?
 shell_log "Creating K8S Cluster result=[" $result "]," "return code=" $ret
 shell_log "Sleep 30 sec to make sure az command completion"; sleep 30
 shell_log "End creaating K8S Cluster: " `date '+%y/%m/%d %H:%M:%S'`
-  
+
 # -- クレデンシャルの取得 ./kube にクレデンシャル情報が書き込まれて kubectl が使えるようになる。
 command="az aks get-credentials --admin --resource-group $AKS_RES_GROUP --name $AKS_CLUSTER_NAME --overwrite-existing"
 shell_log "kubectl 用のクレデンシャルを取得します。["  "$command" "]"
 result=eval $command
 ret=$?
 shell_log "Getting credentials result =" $result "," "return code=" $ret
-shell_Log  "Sleep 10 sec for az command completion"; sleep 10
+shell_log "Sleep 10 sec for az command completion"; sleep 10
 
 # --確認コマンド
 shell_log "check if cluster is created successfully"
@@ -117,3 +124,9 @@ kubectl cluster-info
 shell_log "cluster node check"
 kubectl get node -o=wide
 # kubectl describe node aks-nodepool1-36860460-0
+
+# END TIME
+END_TIME=`date '+%Y/%m/%d %H:%M:%S'`
+
+shell_Log "Budiling practice enviroment complete. start=" $START_TIME ",end=" $END_TIME
+
